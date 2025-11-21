ba4728bdd0fa4879e5b2ac538d5c52f9c5edea4e
ridvanasci <152803025+ridvanasci@users.noreply.github.com>
2025-11-21 16:40:57 +0300

    t

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function SimpleApp() {
+  return <h1>React is Working!</h1>;
+}
+
+createRoot(document.getElementById("root")!).render(<SimpleApp />);
