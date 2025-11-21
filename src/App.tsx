4caf939e0bd9432af1b4f244d127ddc92c3190b3
ridvanasci <152803025+ridvanasci@users.noreply.github.com>
2025-11-21 16:37:54 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,13 +9,13 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <WouterRouter base="/ProphetLifeUI">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/prophet/:id" component={ProphetDetailPage} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/ProphetLifeUI/" component={Home} />
+      <Route path="/ProphetLifeUI/prophet/:id" component={ProphetDetailPage} />
+      <Route path="/" component={Home} />
+      <Route path="/prophet/:id" component={ProphetDetailPage} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
---
+++ src/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TestApp() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>React App is Working!</h1>
+      <p>If you see this, React is loading correctly.</p>
+    </div>
+  );
+}
+
+export default TestApp;
