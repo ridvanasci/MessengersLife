c9808a1006ca424c84d3d94f74bd277e0e5b883a
ridvanasci <152803025+ridvanasci@users.noreply.github.com>
2025-11-21 16:47:02 +0300

    rt

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,11 +9,13 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/prophet/:id" component={ProphetDetailPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/MessengersLife">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/prophet/:id" component={ProphetDetailPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
